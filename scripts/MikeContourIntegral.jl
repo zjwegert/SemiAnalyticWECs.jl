0da1798d423fc6a77d9db5e3f89d64a6b63d753b
zjwegert <60646897+zjwegert@users.noreply.github.com>
2025-11-18 00:23:59 +1000

    done

--- scripts/MikeContourIntegral.jl
+++ scripts/MikeContourIntegral.jl
@@ -1,43 +1,33 @@
-
-h = 1.0
-α = 1.0
-f(μ) = μ*tanh(h*μ) - α
-
-μ0 = first(dispersion_free_surface(α,0,h))*-im
-
-angle(μ0)*180/π
-
-f(μ0)
-
 using FastGaussQuadrature
 using LinearAlgebra
+using QuadGK
+using Roots
+using Roots: Newton
 
-function compute_φ(x,ξ,z,ζ,h,K;order=20)
+include("DispersionEquations.jl")
+
+
+
+function compute_contour_integral(x,ξ,z,ζ,h,K)
   r = sqrt((x-ξ)^2 + (z-ζ)^2)
   r₁ = sqrt((x-ξ)^2 + (z+ζ)^2)
   X = x - ξ
 
-  function F(r)
-    μ = r*(1+im)
-    (1+im)*exp(im*μ*X)/cosh(μ*h)*( cosh(μ*(z+h))*cosh(μ*(ζ+h))/(μ*sinh(μ*h) - K*cosh(μ*h)) + exp(-μ*h)/μ*sinh(μ*z)*sinh(μ*ζ) )
+  function F(R)
+    μ = R*exp(im*π/4)
+    exp(im*π/4)*cos(μ*X)/cosh(μ*h)*( cosh(μ*(z+h))*cosh(μ*(ζ+h))/(μ*sinh(μ*h) - K*cosh(μ*h)) + exp(-μ*h)/μ*sinh(μ*z)*sinh(μ*ζ) )
   end
-  R,w=gausslaguerre(order)
-  I = real(F.(R)⋅w)
+  I = Float64(real(quadgk(F, big(0.0), big(20000), rtol=1e-8, order = 50)[1]))
+  # I = real(quadgk(F, big(0.0), big(40000), rtol=1e-8, order = 50)[1])
+  # I = real(quadgk(F, big(0.0), big(Inf), rtol=1e-12, order = 21)[1]) # Fails because of overflow times underflow
 
   k₀ = first(dispersion_free_surface(K,0,h))
   k = k₀/(-im)
   N₀² = 1/2*(1 + sin(2*k₀*h)/(2*k₀*h))
-  Im_φ = -π/(k₀*h*N₀²)*cosh(k*(z+h))*cosh(k*(ζ+h))*cos(k*X) # Mike says this is residue?
+  Im_φ = -π/(k₀*h*N₀²)*cosh(k*(z+h))*cosh(k*(ζ+h))*cos(k*X)
 
-  return log(r/r₁) .- 2*I + Im_φ
+  return log(r/r₁) - 2*I + Im_φ
 end
-
-# compute_φ(1,0.5,-1,0.5,1,1;order=10)
-# compute_φ(1,0.5,-1,0.5,1,1;order=30)
-# compute_φ(1,0.5,-1,0.5,1,1;order=50)
-# compute_φ(1,0.5,-1,0.5,1,1;order=60)
-# compute_φ(1,0.5,-1,0.5,1,1;order=80)
-compute_φ(1,0.5,-1,0.5,1,1;order=100)
 
 function compute_eigenexpansion(x,ξ,z,ζ,h,K;N=10)
   kₙ = dispersion_free_surface(K,N,h)
@@ -45,4 +35,7 @@ function compute_eigenexpansion(x,ξ,z,ζ,h,K;N=10)
   return -sum(@. π/(kₙ*h*Nₙ²)*cos(kₙ*(z+h))*cos(kₙ*(ζ+h))*exp(-kₙ*abs(x-ξ)))
 end
 
-compute_eigenexpansion(1,0.5,-1,0.5,1,1;N=100)
+Ic = compute_contour_integral(1,0.5,-1,0.5,1,1)
+I_eigen = compute_eigenexpansion(1,0.5,-1,0.5,1,1;N=100)
+
+Ic - I_eigen
