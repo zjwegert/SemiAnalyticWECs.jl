46795ba884e6b5390a9f124e2f3a5bba50f0f953
Zachary J Wegert <60646897+zjwegert@users.noreply.github.com>
2025-12-04 21:20:21 +1030

    Update scripts

--- scripts/PZT_vs_PVDF.jl
+++ scripts/PZT_vs_PVDF.jl
@@ -55,28 +55,28 @@ Ts = collect(range(3,9,2000));
 data = load("$(@__DIR__)/data/pzt_vs_pvdf.jld2")["data"]
 
 fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=4) do
-  fig = Figure(size = (1400, 400),figure_padding = (1,95,1,1))
+  fig = Figure(size = (1400, 400),figure_padding = (1,99,1,1))
   # Energy (surface)
   ax = Axis(fig[2,1],aspect=2,yscale=log10,xlabel="Period (s)",ylabel=L"1 -|R|^2-|T|^2",xticks=3:9)
+  _data = data[data.BC .== "simply_supported" .&& data.Problem .== "submerged" .&& data.Material .== "PVDF",:]
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Submerged (Simply supported)")
+  _data = data[data.BC .== "clamped" .&& data.Problem .== "submerged" .&& data.Material .== "PVDF",:]
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Submerged (Clamped)")
   _data = data[data.BC .== "simply_supported" .&& data.Problem .== "surface" .&& data.Material .== "PVDF",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PVDF (Simply supported)")
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Surface (Simply supported)",linestyle=:dash)
   _data = data[data.BC .== "clamped" .&& data.Problem .== "surface" .&& data.Material .== "PVDF",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PVDF (Clamped)")
-  _data = data[data.BC .== "simply_supported" .&& data.Problem .== "surface" .&& data.Material .== "PZT5H",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PZT5H (Simply supported)",linestyle=:dash)
-  _data = data[data.BC .== "clamped" .&& data.Problem .== "surface" .&& data.Material .== "PZT5H",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PZT5H (Clamped)",linestyle=:dash)
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Surface (Clamped)",linestyle=:dash)
   ylims!(ax,0.8e-4,2.5e-1)
   # Energy (submerged)
   ax = Axis(fig[2,2],aspect=2,yscale=log10,xlabel="Period (s)",ylabel=L"1 -|R|^2-|T|^2",xticks=3:9)
-  _data = data[data.BC .== "simply_supported" .&& data.Problem .== "submerged" .&& data.Material .== "PVDF",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PVDF (Simply supported)")
-  _data = data[data.BC .== "clamped" .&& data.Problem .== "submerged" .&& data.Material .== "PVDF",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PVDF (Clamped)")
   _data = data[data.BC .== "simply_supported" .&& data.Problem .== "submerged" .&& data.Material .== "PZT5H",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PZT5H (Simply supported)",linestyle=:dash)
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Submerged (Simply supported)")
   _data = data[data.BC .== "clamped" .&& data.Problem .== "submerged" .&& data.Material .== "PZT5H",:]
-  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="PZT5H (Clamped)",linestyle=:dash)
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Submerged (Clamped)")
+  _data = data[data.BC .== "simply_supported" .&& data.Problem .== "surface" .&& data.Material .== "PZT5H",:]
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Surface (Simply supported)",linestyle=:dash)
+  _data = data[data.BC .== "clamped" .&& data.Problem .== "surface" .&& data.Material .== "PZT5H",:]
+  lines!(ax,Ts,(1 .-abs.(first(_data.R)).^2 .-abs.(first(_data.T)).^2),label="Surface (Clamped)",linestyle=:dash)
   L = Legend(fig[1,1:2],ax,orientation=:horizontal)#,tellwidth=true)
   ylims!(ax,0.8e-4,2.5e-1)
   # L.nbanks = 2
--- scripts/param_surface_conductance.jl
+++ scripts/param_surface_conductance.jl
@@ -12,46 +12,46 @@ rho0 = 1250;
 Ts = collect(range(3,9,2000));
 
 ## Uncomment to re-run
-# R_s = Vector{ComplexF64}[]; T_s = Vector{ComplexF64}[];
-# P_FFs = Vector{Float64}[]; P_NFs = Vector{Float64}[];
-# mat_names = String[]; bc_names = String[]; problem_names = String[];
-# G_values = Float64[]; G_coeffs = Float64[];
+R_s = Vector{ComplexF64}[]; T_s = Vector{ComplexF64}[];
+P_FFs = Vector{Float64}[]; P_NFs = Vector{Float64}[];
+mat_names = String[]; bc_names = String[]; problem_names = String[];
+G_values = Float64[]; G_coeffs = Float64[];
 
-# for coeff in (0.2,0.5,1,2,5)
-#   for mat in (PZT5H_material_coefficents,)
-#     for bc in (:simply_supported,)
-#       for solver in (solve_submerged_plate_2d,)
-#         ## Mat and Bc
-#         bc_name = string(bc)
-#         mat_name = first(split(string(mat),"_"))
-#         prob_name = split(string(solver),"_")[2]
-#         println("----- Solving $(prob_name) problem, material = $(mat_name), bc = $(bc_name), G_coeff=$coeff -----");
-#         C11,e21,K22,rhop = mat();
-#         dp = 1.1e-4;
-#         G = coeff*K22/(2*dp);
-#         C = K22/(2*dp);
-#         B = d0^3/12*E0/(1-nu0^2) + 2*dp*(dp^2/3+d0*dp/2+d0^2/4)*C11;
-#         η = 1/2*(d0+dp)*e21;
-#         Ib = rho0*d0 + 2*rhop*dp;
+for coeff in (0.2,0.5,1,2,5)
+  for mat in (PVDF_TechMan_material_coefficents,) # PZT5H_material_coefficents
+    for bc in (:simply_supported,)
+      for solver in (solve_submerged_plate_2d,)
+        ## Mat and Bc
+        bc_name = string(bc)
+        mat_name = first(split(string(mat),"_"))
+        prob_name = split(string(solver),"_")[2]
+        println("----- Solving $(prob_name) problem, material = $(mat_name), bc = $(bc_name), G_coeff=$coeff -----");
+        C11,e21,K22,rhop = mat();
+        dp = 1.1e-4;
+        G = coeff*K22/(2*dp);
+        C = K22/(2*dp);
+        B = d0^3/12*E0/(1-nu0^2) + 2*dp*(dp^2/3+d0*dp/2+d0^2/4)*C11;
+        η = 1/2*(d0+dp)*e21;
+        Ib = rho0*d0 + 2*rhop*dp;
 
-#         # Solve
-#         ω = 2π./Ts;
-#         D = @. B + ω*η^2/(im*G+ω*C);
-#         data = @showprogress map(1:length(ω)) do i
-#           solver(ω[i],D[i],Ib,η,G,C,H,h,L,100,ceil(Int,L/0.05);bc_case=bc)
-#         end
+        # Solve
+        ω = 2π./Ts;
+        D = @. B + ω*η^2/(im*G+ω*C);
+        data = @showprogress map(1:length(ω)) do i
+          solver(ω[i],D[i],Ib,η,G,C,H,h,L,100,ceil(Int,L/0.05);bc_case=bc)
+        end
 
-#         push!(R_s, getfield.(data,:R)); push!(T_s, getfield.(data,:T));
-#         push!(P_FFs, getfield.(data,:P_farfield)); push!(P_NFs, getfield.(data,:P_nearfield));
-#         push!(mat_names, mat_name); push!(bc_names, bc_name); push!(problem_names, prob_name);
-#         push!(G_coeffs, coeff); push!(G_values, G);
-#       end
-#     end
-#   end
-# end
+        push!(R_s, getfield.(data,:R)); push!(T_s, getfield.(data,:T));
+        push!(P_FFs, getfield.(data,:P_farfield)); push!(P_NFs, getfield.(data,:P_nearfield));
+        push!(mat_names, mat_name); push!(bc_names, bc_name); push!(problem_names, prob_name);
+        push!(G_coeffs, coeff); push!(G_values, G);
+      end
+    end
+  end
+end
 
-# data = DataFrame("Problem"=>problem_names,"Material"=>mat_names,"BC"=>bc_names,"G_coeff"=>G_coeffs,"G_value"=>G_values,"R"=>R_s,"T"=>T_s,"P_farfield"=>P_FFs,"P_nearfield"=>P_NFs);
-# jldsave("$(@__DIR__)/data/surface_conductance.jld2";data)
+data = DataFrame("Problem"=>problem_names,"Material"=>mat_names,"BC"=>bc_names,"G_coeff"=>G_coeffs,"G_value"=>G_values,"R"=>R_s,"T"=>T_s,"P_farfield"=>P_FFs,"P_nearfield"=>P_NFs);
+jldsave("$(@__DIR__)/data/surface_conductance.jld2";data)
 
 ##############################
 ### Plotting
@@ -59,14 +59,22 @@ Ts = collect(range(3,9,2000));
 data = load("$(@__DIR__)/data/surface_conductance.jld2")["data"]
 
 fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=4) do
-  fig = Figure(size = (1400, 400),figure_padding = (1,95,1,1))
+  fig = Figure(size = (1400, 400),figure_padding = (1,99,1,1))
   ax = Axis(fig[2,1],aspect=2,yscale=log10,xlabel="Period (s)",ylabel=L"1 -|R|^2-|T|^2",xticks=3:9)
-  for _data in eachrow(data)
+  for _data in eachrow(data[data.Material .== "PVDF",:])
     lab = L"G=%$(_data.G_coeff)G_0"
     lines!(ax,Ts,1 .- abs.(_data.R).^2 .- abs.(_data.T).^2,
         label=lab)
   end
-  L = Legend(fig[1,1],ax,orientation=:horizontal)#,tellwidth=true) 
+  ylims!(ax,0.5e-4,5e-1)
+  ax = Axis(fig[2,2],aspect=2,yscale=log10,xlabel="Period (s)",ylabel=L"1 -|R|^2-|T|^2",xticks=3:9)
+  for _data in eachrow(data[data.Material .== "PZT5H",:])
+    lab = L"G=%$(_data.G_coeff)G_0"
+    lines!(ax,Ts,1 .- abs.(_data.R).^2 .- abs.(_data.T).^2,
+        label=lab)
+  end
+  ylims!(ax,0.5e-4,5e-1)
+  L = Legend(fig[1,1:2],ax,orientation=:horizontal)#,tellwidth=true) 
   # L.nbanks = 1
   # resize_to_layout!(fig)
   fig
