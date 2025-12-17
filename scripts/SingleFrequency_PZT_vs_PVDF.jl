740399bc4ae321daa8b141471619bfc012821401
zjwegert <60646897+zjwegert@users.noreply.github.com>
2025-12-17 13:28:26 +1000

    Update figs

--- scripts/SingleFrequency_PZT_vs_PVDF.jl
+++ scripts/SingleFrequency_PZT_vs_PVDF.jl
@@ -32,8 +32,6 @@ XF, x = data.XF, data.x'
 # Plotting
 xf_range = 91:211
 
-lines(x,vec(real(im*∂ₓ²w)))
-
 fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   fig = Figure(size = (1400, 300*3), figure_padding = (0,0,0,0));
   ax11_l = Axis(fig[1,1],width=400,height=300,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(\hat{\zeta})~(m)}",xticks=-10:5:10,
@@ -43,6 +41,7 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   lines!(ax11_l,data.XF[xf_range],vec(real(eta))[xf_range],color=Makie.wong_colors()[1])
   lines!(ax11_r,x,vec(real(w)).-h,color=Makie.wong_colors()[2])
   xlims!(ax11_r,-12.5,12.5)
+  xlims!(ax11_l,-12.5,12.5)
   ylims!(ax11_r,-2.8,1.1)
   ylims!(ax11_l,-2.8,1.1)
   hidespines!(ax11_r)
@@ -51,15 +50,15 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
         minorgrid = false, minorticks = false)
 
   ax21_l = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(M_{11})~(N)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+    yticks=[-300,0,300],ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
   ax21_r = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Re(p)~(Pa)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+    yticks=[-1000,0,1000],ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
   lines!(ax21_l,x,vec(real(M11)),color=Makie.wong_colors()[1])
   lines!(ax21_r,x,vec(real(p)),color=Makie.wong_colors()[2])
   xlims!(ax21_r,-12.5,12.5)
   xlims!(ax21_l,-12.5,12.5)
-  # ylims!(ax41_r,-1500,1500)
-  # ylims!(ax41_l,-1500,1500)
+  ylims!(ax21_r,-1500,1500)
+  ylims!(ax21_l,-450,450)
   hidespines!(ax21_r)
   hidexdecorations!(ax21_r)
   hidexdecorations!(ax21_l;label = true, ticklabels = true, ticks = true, grid = false,
@@ -71,7 +70,7 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   hidexdecorations!(ax31;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax41 = Axis(fig[4,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(q)~(C)}",xticks=-10:5:10)
+  ax41 = Axis(fig[4,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(q)~(Cm^{-2})}",xticks=-10:5:10)
   lines!(ax41,x,vec(real(q)))
   xlims!(ax41,-12.5,12.5)
 
@@ -83,6 +82,7 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   lines!(ax12_l,data.XF[xf_range],vec(imag(eta))[xf_range],color=Makie.wong_colors()[1])
   lines!(ax12_r,x,vec(imag(w)).-h,color=Makie.wong_colors()[2])
   xlims!(ax12_r,-12.5,12.5)
+  xlims!(ax12_l,-12.5,12.5)
   ylims!(ax12_r,-2.8,1.1)
   ylims!(ax12_l,-2.8,1.1)
   hidespines!(ax12_r)
@@ -91,15 +91,15 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
         minorgrid = false, minorticks = false)
 
   ax22_l = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(M_{11})~(N)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+    yticks=[-300,0,300],ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
   ax22_r = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Im(p)~(Pa)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+    yticks=[-600,0,600],ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
   lines!(ax22_l,x,vec(imag(M11)),color=Makie.wong_colors()[1])
   lines!(ax22_r,x,vec(imag(p)),color=Makie.wong_colors()[2])
   xlims!(ax22_r,-12.5,12.5)
   xlims!(ax22_l,-12.5,12.5)
-  # ylims!(ax42_r,-750,750)
-  # ylims!(ax42_l,-750,750)
+  ylims!(ax22_r,-800,800)
+  ylims!(ax22_l,-400,400)
   hidespines!(ax22_r)
   hidexdecorations!(ax22_r)
   hidexdecorations!(ax22_l;label = true, ticklabels = true, ticks = true, grid = false,
@@ -111,7 +111,7 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   hidexdecorations!(ax32;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax42 = Axis(fig[4,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(q)~(C)}",xticks=-10:5:10)
+  ax42 = Axis(fig[4,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(q)~(Cm^{-2})}",xticks=-10:5:10)
   lines!(ax42,x,vec(imag(q)))
   xlims!(ax42,-12.5,12.5)
 
@@ -158,15 +158,15 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
         minorgrid = false, minorticks = false)
 
   ax21_l = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(M_{11})~(N)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+    yticks=[-100,0,100],ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
   ax21_r = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Re(p)~(Pa)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+    yticks=[-1000,0,1000],ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
   lines!(ax21_l,x,vec(real(M11)),color=Makie.wong_colors()[1])
   lines!(ax21_r,x,vec(real(p)),color=Makie.wong_colors()[2])
   xlims!(ax21_r,-12.5,12.5)
   xlims!(ax21_l,-12.5,12.5)
-  # ylims!(ax41_r,-1500,1500)
-  # ylims!(ax41_l,-1500,1500)
+  ylims!(ax21_l,-130,130)
+  ylims!(ax21_r,-1300,1300)
   hidespines!(ax21_r)
   hidexdecorations!(ax21_r)
   hidexdecorations!(ax21_l;label = true, ticklabels = true, ticks = true, grid = false,
@@ -178,7 +178,7 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   hidexdecorations!(ax31;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax41 = Axis(fig[4,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(q)~(C)}",xticks=-10:5:10)
+  ax41 = Axis(fig[4,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(q)~(Cm^{-2})}",xticks=-10:5:10)
   lines!(ax41,x,vec(real(q)))
   xlims!(ax41,-12.5,12.5)
 
@@ -198,15 +198,15 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
         minorgrid = false, minorticks = false)
 
   ax22_l = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(M_{11})~(N)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+    yticks=[-25,0,25],ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
   ax22_r = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Im(p)~(Pa)}",xticks=-10:5:10,
-    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+    yticks=[-200,0,200],ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
   lines!(ax22_l,x,vec(imag(M11)),color=Makie.wong_colors()[1])
   lines!(ax22_r,x,vec(imag(p)),color=Makie.wong_colors()[2])
   xlims!(ax22_r,-12.5,12.5)
   xlims!(ax22_l,-12.5,12.5)
-  # ylims!(ax42_r,-750,750)
-  # ylims!(ax42_l,-750,750)
+  ylims!(ax22_l,-50,70)
+  ylims!(ax22_r,-50*8,70*8)
   hidespines!(ax22_r)
   hidexdecorations!(ax22_r)
   hidexdecorations!(ax22_l;label = true, ticklabels = true, ticks = true, grid = false,
@@ -218,7 +218,7 @@ fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
   hidexdecorations!(ax32;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax42 = Axis(fig[4,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(q)~(C)}",xticks=-10:5:10)
+  ax42 = Axis(fig[4,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(q)~(Cm^{-2})}",xticks=-10:5:10)
   lines!(ax42,x,vec(imag(q)))
   xlims!(ax42,-12.5,12.5)
 
