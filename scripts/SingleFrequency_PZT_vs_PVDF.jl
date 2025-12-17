480f9cc6610791c2f54dc691aa0afa539b5fe0e3
zjwegert <60646897+zjwegert@users.noreply.github.com>
2025-12-17 10:54:25 +1000

    Update fig

--- scripts/SingleFrequency_PZT_vs_PVDF.jl
+++ scripts/SingleFrequency_PZT_vs_PVDF.jl
@@ -9,7 +9,7 @@ nu0 = 0.49;
 E0 = 3.2e+6;
 d0 = 0.01;
 rho0 = 1250;
-Ts = 3;
+Ts = 4;
 
 ## PZT5H
 C11,e21,K22,rhop = PZT5H_material_coefficents()
@@ -30,72 +30,95 @@ M11 = -B*∂ₓ²w + η*v
 p = D*∂ₓ⁴w - ω^2*Ib*w
 XF, x = data.XF, data.x'
 # Plotting
-xf_range = 75:225
+xf_range = 91:211
+
+lines(x,vec(real(im*∂ₓ²w)))
 
 fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
-  fig = Figure(size = (1400, 800), figure_padding = (1,99,1,1));
-  ax11 = Axis(fig[1,1],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Re(w)}",xticks=-15:5:15)
-  lines!(ax11,data.XF[xf_range],vec(real(eta))[xf_range])
-  lines!(ax11,x,vec(real(w)).-h)
-  xlims!(ax11,-16,16)
-  hidexdecorations!(ax11;label = true, ticklabels = true, ticks = true, grid = false,
+  fig = Figure(size = (1400, 300*3), figure_padding = (0,0,0,0));
+  ax11_l = Axis(fig[1,1],width=400,height=300,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(\hat{\zeta})~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax11_r = Axis(fig[1,1],width=400,height=300,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Re(w)-h~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax11_l,data.XF[xf_range],vec(real(eta))[xf_range],color=Makie.wong_colors()[1])
+  lines!(ax11_r,x,vec(real(w)).-h,color=Makie.wong_colors()[2])
+  xlims!(ax11_r,-12.5,12.5)
+  ylims!(ax11_r,-2.8,1.1)
+  ylims!(ax11_l,-2.8,1.1)
+  hidespines!(ax11_r)
+  hidexdecorations!(ax11_r)
+  hidexdecorations!(ax11_l;label = true, ticklabels = true, ticks = true, grid = false,
         minorgrid = false, minorticks = false)
 
-  ax21 = Axis(fig[2,1],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Re(v)}",xticks=-15:5:15)
-  lines!(ax21,x,vec(real(v)))
-  xlims!(ax21,-16,16)
-  hidexdecorations!(ax21;label = true, ticklabels = true, ticks = true, grid = false,
+  ax21_l = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(M_{11})~(N)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax21_r = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Re(p)~(Pa)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax21_l,x,vec(real(M11)),color=Makie.wong_colors()[1])
+  lines!(ax21_r,x,vec(real(p)),color=Makie.wong_colors()[2])
+  xlims!(ax21_r,-12.5,12.5)
+  xlims!(ax21_l,-12.5,12.5)
+  # ylims!(ax41_r,-1500,1500)
+  # ylims!(ax41_l,-1500,1500)
+  hidespines!(ax21_r)
+  hidexdecorations!(ax21_r)
+  hidexdecorations!(ax21_l;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax31 = Axis(fig[3,1],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Re(q)}",xticks=-15:5:15)
-  lines!(ax31,x,vec(real(q)))
-  xlims!(ax31,-16,16)
+  ax31 = Axis(fig[3,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(v)~(V)}",xticks=-10:5:10)
+  lines!(ax31,x,vec(real(v)))
+  xlims!(ax31,-12.5,12.5)
   hidexdecorations!(ax31;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax41 = Axis(fig[4,1],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Re(M_{11})}",xticks=-15:5:15)
-  lines!(ax41,x,vec(real(M11)))
-  xlims!(ax41,-16,16)
-  hidexdecorations!(ax41;label = true, ticklabels = true, ticks = true, grid = false,
-      minorgrid = false, minorticks = false)
+  ax41 = Axis(fig[4,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(q)~(C)}",xticks=-10:5:10)
+  lines!(ax41,x,vec(real(q)))
+  xlims!(ax41,-12.5,12.5)
 
-  ax51 = Axis(fig[5,1],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Re(p)}",xticks=-15:5:15)
-  lines!(ax51,x,vec(real(p)))
-  xlims!(ax51,-16,16)
 
-  ax12 = Axis(fig[1,2],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Im(w)}",xticks=-15:5:15)
-  lines!(ax12,data.XF[xf_range],vec(imag(eta))[xf_range])
-  lines!(ax12,x,vec(imag(w)).-h)
-  xlims!(ax12,-16,16)
-  hidexdecorations!(ax12;label = true, ticklabels = true, ticks = true, grid = false,
+  ax12_l = Axis(fig[1,2],width=400,height=300,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(\hat{\zeta})~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax12_r = Axis(fig[1,2],width=400,height=300,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Im(w)-h~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax12_l,data.XF[xf_range],vec(imag(eta))[xf_range],color=Makie.wong_colors()[1])
+  lines!(ax12_r,x,vec(imag(w)).-h,color=Makie.wong_colors()[2])
+  xlims!(ax12_r,-12.5,12.5)
+  ylims!(ax12_r,-2.8,1.1)
+  ylims!(ax12_l,-2.8,1.1)
+  hidespines!(ax12_r)
+  hidexdecorations!(ax12_r)
+  hidexdecorations!(ax12_l;label = true, ticklabels = true, ticks = true, grid = false,
         minorgrid = false, minorticks = false)
 
-  ax22 = Axis(fig[2,2],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Im(v)}",xticks=-15:5:15)
-  lines!(ax22,x,vec(imag(v)))
-  xlims!(ax22,-16,16)
-  hidexdecorations!(ax22;label = true, ticklabels = true, ticks = true, grid = false,
+  ax22_l = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(M_{11})~(N)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax22_r = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Im(p)~(Pa)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax22_l,x,vec(imag(M11)),color=Makie.wong_colors()[1])
+  lines!(ax22_r,x,vec(imag(p)),color=Makie.wong_colors()[2])
+  xlims!(ax22_r,-12.5,12.5)
+  xlims!(ax22_l,-12.5,12.5)
+  # ylims!(ax42_r,-750,750)
+  # ylims!(ax42_l,-750,750)
+  hidespines!(ax22_r)
+  hidexdecorations!(ax22_r)
+  hidexdecorations!(ax22_l;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax32 = Axis(fig[3,2],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Im(q)}",xticks=-15:5:15)
-  lines!(ax32,x,vec(imag(q)))
-  xlims!(ax32,-16,16)
+  ax32 = Axis(fig[3,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(v)~(V)}",xticks=-10:5:10)
+  lines!(ax32,x,vec(imag(v)))
+  xlims!(ax32,-12.5,12.5)
   hidexdecorations!(ax32;label = true, ticklabels = true, ticks = true, grid = false,
       minorgrid = false, minorticks = false)
 
-  ax42 = Axis(fig[4,2],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Im(M_{11})}",xticks=-15:5:15)
-  lines!(ax42,x,vec(imag(M11)))
-  xlims!(ax42,-16,16)
-  hidexdecorations!(ax42;label = true, ticklabels = true, ticks = true, grid = false,
-      minorgrid = false, minorticks = false)
-
-  ax52 = Axis(fig[5,2],aspect=4,xlabel=L"x",ylabel=L"\mathrm{Im(p)}",xticks=-15:5:15)
-  lines!(ax52,x,vec(imag(p)))
-  xlims!(ax52,-16,16)
+  ax42 = Axis(fig[4,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(q)~(C)}",xticks=-10:5:10)
+  lines!(ax42,x,vec(imag(q)))
+  xlims!(ax42,-12.5,12.5)
 
   fig
 end
 
-save("$(@__DIR__)/figures/TimeDomain_PZT5H_SimplySupported.png",fig;dpi=300)
+save("$(@__DIR__)/figures/SingleFreq_PZT5H_SimplySupported.png",fig;dpi=300)
 
 ## PVDF
 C11,e21,K22,rhop = PVDF_TechMan_material_coefficents()
@@ -110,26 +133,96 @@ Ib = rho0*d0 + 2*rhop*dp;
 D = B + ω*η^2/(im*G+ω*C);
 data = solve_submerged_plate_2d(ω,D,Ib,η,G,C,H,h,L,100,ceil(Int,L/0.025);bc_case=:simply_supported,return_displacements=true)
 
-eta, w, v = data.η_s, data.w, data.v
+eta, w, ∂ₓ²w, ∂ₓ⁴w, v = data.η_s, data.w, data.∂ₓ²w, data.∂ₓ⁴w, data.v
+q = G/(-im*ω)*v
+M11 = -B*∂ₓ²w + η*v
+p = D*∂ₓ⁴w - ω^2*Ib*w
 XF, x = data.XF, data.x'
 # Plotting
-omega_t = 0;
-xf_range = 75:225
+xf_range = 91:211
 
 fig = with_theme(theme_latexfonts(),fontsize=28,linewidth=3) do
-  fig = Figure(size = (1400, 400), figure_padding = (1,99,1,1));
-  ax = Axis(fig[1,1],aspect=4,xlabel=L"x~(\mathrm{m})",ylabel=L"z~(\mathrm{m})",xticks=-15:5:15)
-  lines!(ax,data.XF[xf_range],vec(real(eta))[xf_range])
-  lines!(ax,x,vec(real(w)).-h)
-  xlims!(ax,-16,16)
-  hidexdecorations!(ax;label = true, ticklabels = true, ticks = true, grid = false,
+  fig = Figure(size = (1400, 300*3), figure_padding = (0,0,0,0));
+  ax11_l = Axis(fig[1,1],width=400,height=300,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(\hat{\zeta})~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax11_r = Axis(fig[1,1],width=400,height=300,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Re(w)-h~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax11_l,data.XF[xf_range],vec(real(eta))[xf_range],color=Makie.wong_colors()[1])
+  lines!(ax11_r,x,vec(real(w)).-h,color=Makie.wong_colors()[2])
+  xlims!(ax11_r,-12.5,12.5)
+  ylims!(ax11_r,-3,1.1)
+  ylims!(ax11_l,-3,1.1)
+  hidespines!(ax11_r)
+  hidexdecorations!(ax11_r)
+  hidexdecorations!(ax11_l;label = true, ticklabels = true, ticks = true, grid = false,
         minorgrid = false, minorticks = false)
 
-  ax2 = Axis(fig[2,1],aspect=4,xlabel=L"x~(\mathrm{m})",ylabel="Voltage (V)",xticks=-15:5:15)
-  lines!(ax2,x,vec(real(v)))
-  xlims!(ax2,-16,16)
+  ax21_l = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(M_{11})~(N)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax21_r = Axis(fig[2,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Re(p)~(Pa)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax21_l,x,vec(real(M11)),color=Makie.wong_colors()[1])
+  lines!(ax21_r,x,vec(real(p)),color=Makie.wong_colors()[2])
+  xlims!(ax21_r,-12.5,12.5)
+  xlims!(ax21_l,-12.5,12.5)
+  # ylims!(ax41_r,-1500,1500)
+  # ylims!(ax41_l,-1500,1500)
+  hidespines!(ax21_r)
+  hidexdecorations!(ax21_r)
+  hidexdecorations!(ax21_l;label = true, ticklabels = true, ticks = true, grid = false,
+      minorgrid = false, minorticks = false)
+
+  ax31 = Axis(fig[3,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(v)~(V)}",xticks=-10:5:10)
+  lines!(ax31,x,vec(real(v)))
+  xlims!(ax31,-12.5,12.5)
+  hidexdecorations!(ax31;label = true, ticklabels = true, ticks = true, grid = false,
+      minorgrid = false, minorticks = false)
+
+  ax41 = Axis(fig[4,1],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Re(q)~(C)}",xticks=-10:5:10)
+  lines!(ax41,x,vec(real(q)))
+  xlims!(ax41,-12.5,12.5)
+
+
+  ax12_l = Axis(fig[1,2],width=400,height=300,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(\hat{\zeta})~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax12_r = Axis(fig[1,2],width=400,height=300,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Im(w)-h~(m)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax12_l,data.XF[xf_range],vec(imag(eta))[xf_range],color=Makie.wong_colors()[1])
+  lines!(ax12_r,x,vec(imag(w)).-h,color=Makie.wong_colors()[2])
+  xlims!(ax12_r,-12.5,12.5)
+  ylims!(ax12_r,-3,1.1)
+  ylims!(ax12_l,-3,1.1)
+  hidespines!(ax12_r)
+  hidexdecorations!(ax12_r)
+  hidexdecorations!(ax12_l;label = true, ticklabels = true, ticks = true, grid = false,
+        minorgrid = false, minorticks = false)
+
+  ax22_l = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(M_{11})~(N)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[1],yticklabelcolor=Makie.wong_colors()[1])
+  ax22_r = Axis(fig[2,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",yaxisposition=:right,ylabel=L"\mathrm{Im(p)~(Pa)}",xticks=-10:5:10,
+    ylabelcolor=Makie.wong_colors()[2],yticklabelcolor=Makie.wong_colors()[2])
+  lines!(ax22_l,x,vec(imag(M11)),color=Makie.wong_colors()[1])
+  lines!(ax22_r,x,vec(imag(p)),color=Makie.wong_colors()[2])
+  xlims!(ax22_r,-12.5,12.5)
+  xlims!(ax22_l,-12.5,12.5)
+  # ylims!(ax42_r,-750,750)
+  # ylims!(ax42_l,-750,750)
+  hidespines!(ax22_r)
+  hidexdecorations!(ax22_r)
+  hidexdecorations!(ax22_l;label = true, ticklabels = true, ticks = true, grid = false,
+      minorgrid = false, minorticks = false)
+
+  ax32 = Axis(fig[3,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(v)~(V)}",xticks=-10:5:10)
+  lines!(ax32,x,vec(imag(v)))
+  xlims!(ax32,-12.5,12.5)
+  hidexdecorations!(ax32;label = true, ticklabels = true, ticks = true, grid = false,
+      minorgrid = false, minorticks = false)
+
+  ax42 = Axis(fig[4,2],width=400,height=150,xlabel=L"x~(\mathrm{m})",ylabel=L"\mathrm{Im(q)~(C)}",xticks=-10:5:10)
+  lines!(ax42,x,vec(imag(q)))
+  xlims!(ax42,-12.5,12.5)
 
   fig
 end
 
-save("$(@__DIR__)/figures/TimeDomain_PVDF_SimplySupported.png",fig;dpi=300)
+save("$(@__DIR__)/figures/SingleFreq_PVDF_SimplySupported.png",fig;dpi=300)
